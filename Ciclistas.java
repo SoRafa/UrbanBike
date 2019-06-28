10587ca2bc3afbc8fd292c5ab1a4d15642035c06
Rafael Fortes <43833592+SoRafa@users.noreply.github.com>
2019-06-27 21:07:01 -0300

    Implementando a opção de adicionar e aprimorando o remover...

--- Ciclistas.java
+++ Ciclistas.java
@@ -5,69 +5,68 @@ import java.util.Iterator;
 
 public class Ciclistas implements Imprimivel{
 	
-	ArrayList<PedalPop> listaPop = new ArrayList<PedalPop>();
-	ArrayList<PedalPremium> listaPremium = new ArrayList<PedalPremium>();
-	Iterator<PedalPop> itPop = listaPop.iterator( );
-	Iterator<PedalPremium> itPremium = listaPremium.iterator( );
-	Scanner scan = new Scanner(System.in);
-	int numero = 0;
-	
-	public void inserir(){
-		System.out.println("digite");
-		System.out.println("");
-		listaPop.add();
-		}
+    ArrayList<ContaUrbanBike> listaPedais = new ArrayList<>();
+    Iterator<ContaUrbanBike> itPedais = listaPedais.iterator();
+    Scanner scan = new Scanner(System.in);
+    int numero = 0;
+    double saldo = 0;
+    
+    public void inserir(){
+        System.out.println("Digite o tipo de conta que deseja criar: 1 - PedalPop\t 2 - PedalPremium");
+        numero = Integer.parseInt(scan.nextLine());
+        if(numero == 1){
+           PedalPop Pedal = new PedalPop();
+           System.out.println("Digite a quantidade que deseja depositar:");
+           saldo = scan.nextDouble();
+           Pedal.setCarteira(saldo);
+           int i = 0; i++;
+           Pedal.setConta(i);
+        }
+        else if(numero == 2){
+           PedalPremium Pedal = new PedalPremium();
+           System.out.println("Digite a quantidade que deseja depositar: (minimo de "+ Pedal.getLimite()+"reais)");
+           saldo = scan.nextDouble();
+           Pedal.setCarteira(saldo);
+           int i = 0; i++;
+           Pedal.setConta(i);
+        }else{
+            System.out.println("OpÃ§Ã£o invÃ¡lida"); 
+        }
+	System.out.println("Digite a quantidade que deseja depositar:");
+	listaPedais.add();
+	}
 
-	public void remover(){
-		numero = Integer.parseInt(scan.nextLine());
-		for(int i = 0; i < listaPop.size(); i++) { 
-			
-			while ( itPop.hasNext() ) {						//laço para pesquisar no pop
-				System.out.println(itPop.next().getConta());
-				if( itPop.next().getConta() == numero) {
-					listaPop.remove(i);
-					}
-				}
-				while ( itPremium.hasNext() ) {				//laço para pesquisar no premium
-					System.out.println(itPremium.next().getConta());
-					if( itPremium.next().getConta() == numero) {
-					listaPremium.remove(i);
-					}
-				}
-			}
-		}
+    public void remover(ContaUrbanBike c){
+	while ( itPedais.hasNext() ) {                              
+            System.out.println(itPedais.next().getConta());
+            if( itPedais.next() == c) {
+                listaPedais.remove(itPedais.next());
+                }
+            }
+        }
 
-	public void procurarPedal(){
-		numero = Integer.parseInt(scan.nextLine());
-			for(int i = 0; i < listaPop.size(); i++) { 
-			
-			while ( itPop.hasNext() ) {						//laço para pesquisar no pop
-				System.out.println(itPop.next().getConta());
-				if( itPop.next().getConta() == numero) {
-					listaPop.get(i);
-					}
-				}
-				while ( itPremium.hasNext() ) {				//laço para pesquisar no premium
-					System.out.println(itPremium.next().getConta());
-					if( itPremium.next().getConta() == numero) {
-					listaPremium.get(i);
-					}
-				}
-			}
-		}
+	public ContaUrbanBike procurarPedal(int num){
+		while ( itPedais.hasNext() ) {				//laÃ§o para pesquisar 
+			if( itPedais.next().getConta() == numero) {
+                            System.out.println(itPedais.next().getConta());
+                            return itPedais.next();
+                        }
+	}
+        return null;
+    }
 	
 	@Override
 	public void mostraDados() {
 			
 	}
 	
-	public void menu1() { // Menu da Aplicação
+	public void menu1() { // Menu da AplicaÃ§Ã£o
 		int opcao = 0;
 		do {
 			System.out.println("\n         ========================================");
 			System.out.println("          1 - Criar pedal        "); 
 			System.out.println("          2 - Remover Pedal      ");
-			System.out.println("          3 - Gerar relatório    ");
+			System.out.println("          3 - Gerar relatÃ³rio    ");
 			System.out.println("          4 - Selecionar Pedal      ");
 			System.out.println("          5 - Finalizar ");
 			System.out.println("          ========================================\n");			
@@ -75,18 +74,17 @@ public class Ciclistas implements Imprimivel{
                 opcao = Integer.parseInt(scan1.nextLine());
 			switch (opcao) {
 			case 1://debug
-				while ( itPop.hasNext( ) ) {
-					System.out.println( itPop.next( ) );
+                        numero = Integer.parseInt(scan.nextLine()); 
+				while ( itPedais.hasNext( ) ) {
+					System.out.println( itPedais.next( ) );
 					}
-				while ( itPremium.hasNext( ) ) {
-					System.out.println( itPremium.next( ) );
-					}
-                            ;
 			break;
 		        case 2:
+                        numero = Integer.parseInt(scan.nextLine()); 
                             ;
 			break;
 		        case 3:
+                        numero = Integer.parseInt(scan.nextLine()); 
                            	;
             break;
 		        case 4:
@@ -95,23 +93,23 @@ public class Ciclistas implements Imprimivel{
 				case 0:
             break;
 				default:
-            System.out.println("Opção Inválida!");
+            System.out.println("OpÃ§Ã£o InvÃ¡lida!");
             break;
             }
         } while (opcao != 0);
     }
 	
-	public void menu2() { // Menu da opção 4
+	public void menu2() { // Menu da opÃ§Ã£o 4
 		int opcao = 0;
 		do {
 			System.out.println("\n         ========================================");
 			System.out.println("          A - Creditar        "); 
 			System.out.println("          B - Pedalar      ");
 			System.out.println("          C - Transferir    ");
-			System.out.println("          D - Gerar relatório      ");
+			System.out.println("          D - Gerar relatÃ³rio      ");
 			System.out.println("          E - Retornar ao menu anterior ");
 			System.out.println("          ========================================\n");			
-				Scanner scan2 = new Scanner(System.in);
+                Scanner scan2 = new Scanner(System.in);
             	opcao = Integer.parseInt(scan2.nextLine());
 			switch (opcao) {
 			case 1:
@@ -129,7 +127,7 @@ public class Ciclistas implements Imprimivel{
 				case 0:
             break;
 				default:
-            System.out.println("Opção Inválida!");
+            System.out.println("OpÃ§Ã£o InvÃ¡lida!");
             break;
             }
         } while (opcao != 0);
